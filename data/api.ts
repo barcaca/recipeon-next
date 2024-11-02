81c6d704568a8389f058bb058bd0e4e0afc0e6b2
barcaca <73846225+barcaca@users.noreply.github.com>
2024-11-02 13:57:55 -0300

    feat: cria função fetchRecipes para buscar receitas da API

---
+++ data/api.ts
@@ -0,0 +1,29 @@
+import type { TReceita } from '@/types/recipe'
+import { BASE_URL } from './base'
+
+/**
+ * Fetches recipes based on type, page, limit, and optional search query.
+ *
+ * @param {string} type - The category of recipes to fetch.
+ * @param {number} page - The page number for pagination.
+ * @param {number} [limit=8] - The number of recipes per page.
+ * @param {string} [search] - The search term to filter recipes by name.
+ * @returns {Promise<TReceita[]>} - A promise that resolves to an array of recipes.
+ */
+async function fetchRecipes(
+  type: string,
+  page = 1,
+  limit = 8,
+  search?: string
+): Promise<TReceita[]> {
+  const url = search
+    ? `${BASE_URL.REST}?nome_like=${search}`
+    : `${BASE_URL.REST}?categoria_like=${type}&_page=${page}&_limit=${limit}`
+
+  const response = await fetch(url)
+  const data: TReceita[] = await response.json()
+
+  return data
+}
+
+export const getRecipes = fetchRecipes
---
+++ data/base.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = {
+  REST: 'https://recipeon-backend.vercel.app/api/receitas',
+} as const
