1ff6383994d5db3fd68d717f5c6eb446a8b34546
barcaca <73846225+barcaca@users.noreply.github.com>
2024-11-02 14:03:51 -0300

    feat: integra busca de receitas com parâmetros dinâmicos na Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
- return(
-  <div>Ola mundo</div>
- )
+import { HeaderMain } from '@/components/header-main'
+import { RecipeButtonsGroups } from '@/components/recipe-buttons-groups'
+import { getRecipes } from '@/data/api'
+
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+
+export default async function Home({ searchParams }: { searchParams: SearchParams }) {
+  const type = (await searchParams).type || 'breakfast'
+  const search = (await searchParams).search || undefined
+  const isSearch = search ? 'search' : type
+  const data = await getRecipes(type as string, 1, 8, search as string)
+
+  return (
+    <main className="mt-6 flex w-full flex-col items-center">
+      <div className="w-full max-w-screen-lg px-4 lg:px-8">
+        <HeaderMain type={isSearch as string} />
+        <RecipeButtonsGroups type={isSearch as string} />
+        {data[0].nome}
+      </div>
+    </main>
+  )
 }
