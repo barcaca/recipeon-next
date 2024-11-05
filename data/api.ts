ff1005c35f1b3f132028a4bcdc092ec542e32070
barcaca <73846225+barcaca@users.noreply.github.com>
2024-11-05 14:15:28 -0300

    chore: ajusta API para retornar total de receitas

--- data/api.ts
+++ data/api.ts
@@ -10,23 +10,25 @@ import { BASE_URL } from './base'
  * @param {number} page - The page number for pagination.
  * @param {number} [limit=8] - The number of recipes per page.
  * @param {string} [search] - The search term to filter recipes by name.
- * @returns {Promise<TReceita[]>} - A promise that resolves to an array of recipes.
+ * @returns {Promise<{ recipes: TReceita[]; totalRecipes: number }>} - A promise that resolves to an object containing an array of recipes and the total number of recipes.
  */
 async function fetchRecipes(
   type: string,
   page = 1,
   limit = 8,
   search?: string
-): Promise<TReceita[]> {
+): Promise<{ recipes: TReceita[]; totalRecipes: number }> {
   noStore()
   const url = search
-    ? `${BASE_URL.REST}?nome_like=${search}`
+    ? `${BASE_URL.REST}?nome_like=${search}&_page=${page}&_limit=${limit}`
     : `${BASE_URL.REST}?categoria_like=${type}&_page=${page}&_limit=${limit}`
 
   const response = await fetch(url)
-  const data: TReceita[] = await response.json()
+  const recipes: TReceita[] = await response.json()
 
-  return data
+  const totalRecipes = Number.parseInt(response.headers.get('X-Total-Count') || '0', 10)
+
+  return { recipes, totalRecipes }
 }
 
 export const getRecipes = fetchRecipes
