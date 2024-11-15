0102a493a41302d94dd0930faf4863d114caf8e2
barcaca <73846225+barcaca@users.noreply.github.com>
2024-11-15 14:32:38 -0300

    fix: ajusta schema.ts para criação

--- actions/receita/criar/schema.ts
+++ actions/receita/criar/schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-const MAX_FILE_SIZE = 5000000
+const MAX_FILE_SIZE = 3000000
 const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp']
 const UNIDADES = [
   'mililitros',
@@ -39,34 +39,35 @@ const formSchema = z.object({
     .min(1, 'O nome da receita deve ter pelo menos 1 caractere.'),
   imagem: z
     .instanceof(File, { message: 'Escolha uma imagem.' })
-    .refine(file => file.size <= MAX_FILE_SIZE, 'O tamanho máximo do arquivo é 5 MB.')
+    .refine(file => file.size <= MAX_FILE_SIZE, 'O tamanho máximo do arquivo é 3 MB.')
     .refine(
       file => ACCEPTED_IMAGE_TYPES.includes(file.type),
       'Arquivos .jpg, .jpeg, .png e .webp são aceitos.'
     ),
-  serve: z
-    .string()
+  serve: z.coerce
+    .number()
     .refine(value => Number(value) > 0, 'Serve deve ser um número positivo.')
     .refine(value => Number(value) >= 1, 'Serve deve ser maior ou igual a 1.')
-    .refine(value => Number(value) <= 100, 'Serve deve ser menor ou igual a 100.')
-    .pipe(z.coerce.number()),
-  tempo: z
-    .string()
+    .refine(value => Number(value) <= 100, 'Serve deve ser menor ou igual a 100.'),
+  tempo: z.coerce
+    .number()
     .refine(value => Number(value) > 0, 'Tempo deve ser um número positivo.')
     .refine(value => Number(value) >= 1, 'Tempo deve ser maior ou igual a 1.')
-    .refine(value => Number(value) <= 1000, 'Tempo deve ser menor ou igual a 1000.')
-    .pipe(z.coerce.number()),
+    .refine(value => Number(value) <= 1000, 'Tempo deve ser menor ou igual a 1000.'),
   descricao: z
     .string({ required_error: 'A descrição da receita é obrigatória.' })
-    .min(10, 'A descrição da receita deve ter pelo menos 10 caracteres.'),
-  categorias: z.string().array().nonempty('Escolha pelo menos uma categoria.'),
+    .min(5, 'A descrição da receita deve ter pelo menos 5 caracteres.'),
+  categoria: z.string().array().nonempty('Escolha pelo menos uma categoria.'),
   ingredientes: z.array(ingredientesSchema),
   passos: z
     .string()
-    .min(10, 'O passo deve ter pelo menos 10 caractere.')
+    .min(5, 'O passo deve ter pelo menos 5 caractere.')
     .array()
     .nonempty('Adicione pelo menos um passo.'),
 })
 
 export const CreateRecipeSchema = formSchema
 export type TCreateRecipeData = z.infer<typeof CreateRecipeSchema>
+export type TCreateRecipeDataAfterValidation = Omit<TCreateRecipeData, 'imagem'> & {
+  imagem: string
+}
